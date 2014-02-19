4f2fac1ee5dc392cd52054672eeb5eb2f13a34ed
Dominic McGinnis <dominicmcginnis@gmail.com>
2014-02-19 10:04:16 -0800

    SiteToLink was not being properly substitiued during the curl call, so moved inline

--- createLikeData.sh
+++ createLikeData.sh
@@ -9,7 +9,6 @@
 ### ====================================================================== ###
 #
 FB_URL='https://api.facebook.com/method/fql.query'
-SITE_TO_LIKE='http://www.facebook.com/*****'
 OFILE='fbLikes.xml'
 ERROR_FLAG=0
 ERROR_LOG=messages
@@ -25,7 +24,7 @@ error() {
  }
 
 # curl call for StubHub US
-/usr/bin/curl --get "$FB_URL" --data 'query=select%20%20like_count%20from%20link_stat%20where%20url=%22$SITE_TO_LIKE%22' > "$OFILE" 2> /tmp/$$-US.log && /bin/rm -f /tmp/$$-US.log || ERROR_FLAG=1
+/usr/bin/curl --get "$FB_URL" --data 'query=select%20%20like_count%20from%20link_stat%20where%20url=%22http://www.facebook.com/*******%22' > "$OFILE" 2> /tmp/$$-US.log && /bin/rm -f /tmp/$$-US.log || ERROR_FLAG=1
 
 OFILE_WC=`/usr/bin/wc -w $OFILE | awk '{print $1}'`
 
